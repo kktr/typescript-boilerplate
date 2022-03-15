d442e76dd0ef956a102670ddec3abe394124f534
Krystian <sogestpodgorna4@gmail.com>
2022-03-15 18:33:29 +0100

    chore: delete plugin:prettier/recommended

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,6 @@ module.exports = {
     'plugin:unicorn/recommended',
     'plugin:import/recommended',
     'plugin:import/typescript',
-    'plugin:prettier/recommended',
     'prettier',
   ],
   env: {
